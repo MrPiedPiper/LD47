f37dde5fef7f604fd7ecce4324161f39740f5f8a
MrPiedPiper <35707072+MrPiedPiper@users.noreply.github.com>
2020-10-03 00:51:48 -0700

    Add chicken player and fly depth

--- scenes/Enemy/Enemy.gd
+++ scenes/Enemy/Enemy.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+signal move_to_front
+signal move_to_back
+
 export(NodePath) var path
 
 var speed = 8
@@ -11,10 +14,20 @@ var touching_enemies = []
 
 onready var curve = get_node(path).curve
 var points = []
+var midway
+var is_front = false
 
 func _ready():
+	var highest
+	var lowest
 	for i in range(curve.get_point_count()):
-		points.append(curve.get_point_position(i))
+		var curr_point = curve.get_point_position(i)
+		points.append(curr_point)
+		if highest == null or highest < curr_point.y:
+			highest = curr_point.y
+		if lowest == null or highest > curr_point.y:
+			lowest = curr_point.y
+	midway = (highest + lowest)/2
 	target = points[0]
 
 func _physics_process(delta):
@@ -28,6 +41,15 @@ func _physics_process(delta):
 	if position.distance_to(target) < 100:
 		points.insert(0,points.pop_back())
 		target = points[0]
+		print(str("target: ",target.y," mid: ",midway))
+		if target.y > midway and !is_front:
+			print("go front")
+			is_front = true
+			emit_signal("move_to_front",self)
+		elif target.y < midway and is_front:
+			print("go back")
+			emit_signal("move_to_back",self)
+			is_front = false
 
 func _on_Area2D_area_entered(area):
 	if area.owner.is_in_group("enemy"):
--- scenes/player/PlayerStretch/PlayerStretch.gd
+++ scenes/player/PlayerStretch/PlayerStretch.gd
@@ -2,6 +2,7 @@ extends "res://scenes/player/Player.gd"
 
 export var stretch_base_pos = Vector2(0,0)
 export var stretch_default_pos = Vector2(0,0)
+
 onready var stretch = $Stretchie/Stretch
 onready var end = $Stretchie/End
 onready var stretch_timer = $StretchTimer
@@ -9,16 +10,17 @@ onready var stretch_timer = $StretchTimer
 func _ready():
 	type = TYPE.STRETCH
 	stretch.position = stretch_base_pos
+	stretch_to(stretch_default_pos)
 
 func clicked(pos):
 	stretch_to(pos)
+	stretch_timer.start()
 
 func stretch_to(pos):
 	end.global_position = pos
 	var distance = stretch_base_pos.distance_to(end.position)
 	stretch.rotation = stretch.position.angle_to_point(end.position)-PI/2
 	stretch.scale.y = distance/stretch.get_rect().size.y
-	stretch_timer.start()
 
 func _on_StretchTimer_timeout():
 	stretch_to(stretch_default_pos)
