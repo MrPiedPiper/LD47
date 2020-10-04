ea18e3deaac3d576fa6a84822db6eef4db28d880
MrPiedPiper <35707072+MrPiedPiper@users.noreply.github.com>
2020-10-04 13:23:38 -0700

    Add three number display to utilities, and add fancy score display on the main menu

--- scenes/MainMenu/MainMenu.gd
+++ scenes/MainMenu/MainMenu.gd
@@ -2,16 +2,12 @@ extends Control
 
 signal play_game_pressed
 
-onready var score_label = $MarginContainer/VBoxContainer/HBoxContainer/MarginContainer/PanelContainer/HBoxContainer/Label2
+onready var last_score_display = $MarginContainer/VBoxContainer/HBoxContainer/CenterContainer/MarginContainer/PanelContainer/HBoxContainer/VBoxContainer/ThreeNumberDisplayHigh
+onready var high_score_display = $MarginContainer/VBoxContainer/HBoxContainer/CenterContainer/MarginContainer/PanelContainer/HBoxContainer/VBoxContainer/ThreeNumberDisplayLast
 
 func _on_LetsRollButton_button_up():
 	emit_signal("play_game_pressed")
 
 func update_score():
-	var score_string = str(Utility.score)
-	while score_string.length() > 3:
-		score_string = "0"+score_string
-	var high_score_string = str(Utility.high_score)
-	while high_score_string.length() > 3:
-		high_score_string = "0"+high_score_string
-	score_label.text = score_string+"\n"+high_score_string
+	last_score_display.set_numbers(Utility.score)
+	high_score_display.set_numbers(Utility.high_score)
